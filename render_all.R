7249bd9e075ecd95995ef2359049261985f03c46
Max Domagk <max.domagk@tu-dresden.de>
2026-09-09 13:57:00 +0200

    Disable citeproc warnings

--- render_all.R
+++ render_all.R
@@ -1,4 +1,4 @@
-rmarkdown::render("CV_Domagk_Max.Rmd", output_file = "CV_Domagk_Max.pdf")
+rmarkdown::render("CV_Domagk_Max.Rmd", output_file = "CV_Domagk_Max.pdf", quiet = TRUE)
 rmarkdown::render("CV_Domagk_Max.Rmd",
                    params = list(full = TRUE),
-                   output_file = "CV_Domagk_Max_full.pdf")
+                   output_file = "CV_Domagk_Max_full.pdf", quiet = TRUE)
